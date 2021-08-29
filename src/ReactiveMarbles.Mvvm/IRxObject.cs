da12383e7db24a88b8d68c4f7db958520d841f7c
rlittlesii <6969701+RLittlesII@users.noreply.github.com>
2021-08-29 02:32:27 -0500

    feature: Added implementation for RxObject

--- src/ReactiveMarbles.Mvvm/IRxObject.cs
+++ src/ReactiveMarbles.Mvvm/IRxObject.cs
@@ -3,14 +3,7 @@
 // See the LICENSE file in the project root for full license information.
 
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
-using System.Reactive;
-using System.Reactive.Disposables;
-using System.Reactive.Subjects;
-using System.Runtime.CompilerServices;
-using System.Threading;
 
 namespace ReactiveMarbles.Core
 {
@@ -23,189 +16,49 @@ namespace ReactiveMarbles.Core
     public interface IRxObject : INotifyPropertyChanged, INotifyPropertyChanging
     {
         /// <summary>
-        /// Gets internal thrown exceptions.
-        /// </summary>
-        protected internal Lazy<ISubject<Exception>> ThrownExceptionsInternal { get; }
-
-        /// <summary>
         /// Gets a observable for when an exception is thrown.
         /// </summary>
         IObservable<Exception> ThrownExceptions { get; }
 
         /// <summary>
-        /// Gets the change state of the object.
+        /// Gets an observable that fires *before* a property is about to
+        /// be changed. Note that this should not fire duplicate change notifications if a
+        /// property is set to the same value multiple times.
         /// </summary>
-        protected internal Lazy<ChangeState> State { get; }
+        IObservable<RxPropertyChangingEventArgs<IRxObject>> Changing { get; }
 
         /// <summary>
-        /// Raises a property changed event.
+        /// Gets an Observable that fires *after* a property has changed.
+        /// Note that this should not fire duplicate change notifications if a
+        /// property is set to the same value multiple times.
         /// </summary>
-        /// <param name="propertyName">The name of the property that has changed.</param>
-        public virtual void RaisePropertyChanging([CallerMemberName] string propertyName = "")
-        {
-            if (!AreChangeNotificationsEnabled())
-            {
-                return;
-            }
-
-            try
-            {
-                RaisePropertyChanging(new PropertyChangingEventArgs(propertyName));
-            }
-            catch (Exception e)
-            {
-                if (ThrownExceptionsInternal.IsValueCreated)
-                {
-                    ThrownExceptionsInternal.Value.OnNext(e);
-                }
-            }
-        }
-
-        /// <summary>
-        /// Raises a property changed event.
-        /// </summary>
-        /// <param name="propertyName">The name of the property that has changed.</param>
-        public virtual void RaisePropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            if (!AreChangeNotificationsEnabled())
-            {
-                return;
-            }
-
-            try
-            {
-                RaisePropertyChanged(new PropertyChangedEventArgs(propertyName));
-            }
-            catch (Exception e)
-            {
-                if (ThrownExceptionsInternal.IsValueCreated)
-                {
-                    ThrownExceptionsInternal.Value.OnNext(e);
-                }
-            }
-        }
-
-        /// <summary>
-        /// RaiseAndSetIfChanged fully implements a Setter for a read-write
-        /// property on a ReactiveObject, using CallerMemberName to raise the notification
-        /// and the ref to the backing field to set the property.
-        /// </summary>
-        /// <typeparam name="T">The type of the value.</typeparam>
-        /// <param name="backingField">A Reference to the backing field for this
-        /// property.</param>
-        /// <param name="newValue">The new value.</param>
-        /// <param name="propertyName">The name of the property, usually
-        /// automatically provided through the CallerMemberName attribute.</param>
-        void RaiseAndSetIfChanged<T>(
-            ref T backingField,
-            T newValue,
-            [CallerMemberName] string? propertyName = null)
-        {
-            if (propertyName is null)
-            {
-                throw new ArgumentNullException(nameof(propertyName));
-            }
-
-            if (EqualityComparer<T>.Default.Equals(backingField, newValue))
-            {
-                return;
-            }
-
-            RaisePropertyChanging(propertyName);
-            backingField = newValue;
-            RaisePropertyChanged(propertyName);
-        }
-
-        /// <summary>
-        /// Raise a property is changing event.
-        /// </summary>
-        /// <param name="args">The arguments with details about the property that is changing.</param>
-        void RaisePropertyChanging(PropertyChangingEventArgs args);
-
-        /// <summary>
-        /// Raise a property has changed event.
-        /// </summary>
-        /// <param name="args">The arguments with details about the property that has changed.</param>
-        void RaisePropertyChanged(PropertyChangedEventArgs args);
-
-        /// <summary>
-        /// Gets a value indicating whether the change notifications are enabled.
-        /// </summary>
-        /// <returns>A truth value.</returns>
-        public bool AreChangeNotificationsEnabled() => !State.IsValueCreated || Interlocked.Read(ref State.Value.ChangeNotificationsSuppressed) == 0;
-
-        /// <summary>
-        /// Gets a value indicating whether the change notifications are delayed.
-        /// </summary>
-        /// <returns>A truth value.</returns>
-        public bool AreChangeNotificationsDelayed() => State.IsValueCreated && Interlocked.Read(ref State.Value.ChangeNotificationsDelayed) > 0;
+        IObservable<RxPropertyChangedEventArgs<IRxObject>> Changed { get; }
 
         /// <summary>
         /// When this method is called, an object will not fire change
         /// notifications (neither traditional nor Observable notifications)
         /// until the return value is disposed.
-        /// If this method is called multiple times it will reference count
-        /// and not perform notification until all values returned are disposed.
         /// </summary>
         /// <returns>An object that, when disposed, re-enables change
         /// notifications.</returns>
-        public IDisposable SuppressChangeNotifications()
-        {
-            Interlocked.Increment(ref State.Value.ChangeNotificationsSuppressed);
-            return Disposable.Create(() => Interlocked.Decrement(ref State.Value.ChangeNotificationsSuppressed));
-        }
+        IDisposable SuppressChangeNotifications();
 
         /// <summary>
-        /// When this method is called, an object will not dispatch change
-        /// Observable notifications until the return value is disposed.
-        /// When the Disposable it will dispatched all queued notifications.
-        /// If this method is called multiple times it will reference count
-        /// and not perform notification until all values returned are disposed.
+        /// Determines if change notifications are enabled or not.
         /// </summary>
-        /// <returns>An object that, when disposed, re-enables Observable change
-        /// notifications.</returns>
-        public IDisposable DelayChangeNotifications()
-        {
-            if (Interlocked.Increment(ref State.Value.ChangeNotificationsDelayed) == 1)
-            {
-                if (State.IsValueCreated)
-                {
-                    State.Value.StartOrStopDelayingChangeNotifications.OnNext(Unit.Default);
-                }
-            }
-
-            return Disposable.Create(() =>
-            {
-                if (Interlocked.Decrement(ref State.Value.ChangeNotificationsDelayed) == 0)
-                {
-                    if (State.IsValueCreated)
-                    {
-                        State.Value.StartOrStopDelayingChangeNotifications.OnNext(Unit.Default);
-                    }
-                }
-            });
-        }
+        /// <returns>A value indicating whether change notifications are enabled.</returns>
+        bool AreChangeNotificationsEnabled();
 
         /// <summary>
-        /// Represents the changed state of an <see cref="IRxObject"/>.
+        /// Gets a value indicating whether the change notifications are delayed.
         /// </summary>
-        [SuppressMessage("SA1401", "SA1401", Justification = "Deliberate Field usage for default interface implementation.")]
-        protected internal class ChangeState
-        {
-            /// <summary>
-            /// Gets the change notification delays.
-            /// </summary>
-            public long ChangeNotificationsDelayed;
+        /// <returns>A truth value.</returns>
+        bool AreChangeNotificationsDelayed();
 
-            /// <summary>
-            /// Gets the change notification suppression.
-            /// </summary>
-            public long ChangeNotificationsSuppressed;
-
-            /// <summary>
-            /// Gets the delayed change notification subject.
-            /// </summary>
-            public Subject<Unit> StartOrStopDelayingChangeNotifications { get; } = new Subject<Unit>();
-        }
+        /// <summary>
+        /// Delays notifications until the return IDisposable is disposed.
+        /// </summary>
+        /// <returns>A disposable which when disposed will send delayed notifications.</returns>
+        IDisposable DelayChangeNotifications();
     }
 }
